dd2e56d1d15fa632e3ba1a8addfc16ccba87a014
John Osberg <john.osberg@smartthings.com>
2020-05-04 09:25:12 -0500

    Added Hobby classifier and rough path forward for future classifiers

---
+++ src/main/kotlin/blackmichael/discord/command/classifier/PotatoClassifier.kt
@@ -0,0 +1,26 @@
+package blackmichael.discord.command.classifier
+
+import com.jessecorbett.diskord.util.EnhancedEventListener
+import kotlin.random.Random
+
+class PotatoClassifier(
+    private val botId: String
+) : Classifier {
+
+    override fun classify(subject: String): Boolean {
+        return if (subject.trim() == botId.trim()) {
+            true
+        } else {
+            Random.nextBoolean()
+        }
+    }
+
+    override fun getAffirmativeMessage(subject: String): String {
+        return "Yes, $subject is a potato."
+    }
+
+    override fun getNegativeMessage(subject: String): String {
+        return "Nah, $subject is not a potato."
+    }
+}
+

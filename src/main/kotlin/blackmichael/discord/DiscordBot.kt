eba141b77fb234d32a2da9382ae232b16352c92f
Michael.Black2 <Michael.Black2@Target.com>
2020-05-05 14:56:48 -0500

    Fixed app graceful shutdown

--- src/main/kotlin/blackmichael/discord/DiscordBot.kt
+++ src/main/kotlin/blackmichael/discord/DiscordBot.kt
@@ -6,10 +6,17 @@ import blackmichael.discord.command.classifier.subjectClassifierCommand
 import blackmichael.discord.command.helpCommand
 import blackmichael.discord.command.pingPongCommand
 import blackmichael.discord.command.sourceCodeCommand
-import com.jessecorbett.diskord.dsl.bot
+import com.jessecorbett.diskord.dsl.Bot
 import com.jessecorbett.diskord.dsl.commands
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
+import java.io.Closeable
 
-class DiscordBot(val config: Config) {
+class DiscordBot(val config: Config) : Closeable {
     data class Config(
         val token: String,
         val prefix: String,
@@ -17,18 +24,38 @@ class DiscordBot(val config: Config) {
         val sourceCode: SourceCode.Config
     )
 
-    suspend fun start() {
-        bot(config.token) {
-            commands(config.prefix) {
-                val commands: List<Command> = listOf(
-                    pingPongCommand(this),
-                    subjectClassifierCommand(this, config.botId),
-                    sourceCodeCommand(this, config.sourceCode)
-                )
+    private val log = LoggerFactory.getLogger(javaClass)
 
-                commands.forEach { it.listen() }
-                helpCommand(this, commands).listen()
-            }
+    private var _job: Job? = null
+    private val job: Job
+        get() = _job ?: throw IllegalStateException("job is not initialized")
+
+    private val bot = Bot(config.token).apply {
+        commands(config.prefix) {
+            val commands: List<Command> = listOf(
+                pingPongCommand(this),
+                subjectClassifierCommand(this, config.botId),
+                sourceCodeCommand(this, config.sourceCode)
+            )
+
+            commands.forEach { it.listen() }
+            helpCommand(this, commands).listen()
         }
+    }
+
+    suspend fun start() {
+        log.info("starting discord bot")
+        _job = GlobalScope.launch(Dispatchers.IO) {
+            bot.start()
+        }
+    }
+
+    override fun close() {
+        log.info("closing discord bot")
+        runBlocking {
+            bot.shutdown()
+            job.join()
+        }
+        _job = null
     }
 }
--- src/main/kotlin/blackmichael/discord/Main.kt
+++ src/main/kotlin/blackmichael/discord/Main.kt
@@ -3,18 +3,35 @@ package blackmichael.discord
 import blackmichael.discord.server.SimpleServer
 import com.typesafe.config.ConfigFactory
 import io.github.config4k.extract
+import kotlinx.coroutines.runBlocking
 import kotlin.system.exitProcess
 
-suspend fun main() {
+fun main() {
     val config = ConfigFactory.load()
     val bot = DiscordBot(config.extract("bot"))
     val server = SimpleServer(config.extract("server"))
 
-    Runtime.getRuntime().addShutdownHook(Thread {
-        server.close()
-        exitProcess(1)
-    })
+    suspend fun start() {
+        server.start()
+        bot.start()
+    }
 
-    server.start()
-    bot.start()
+    fun close() {
+        bot.close()
+        server.close()
+    }
+
+    try {
+        Runtime.getRuntime().addShutdownHook(Thread {
+            close()
+            exitProcess(0)
+        })
+
+        runBlocking {
+            start()
+        }
+    } catch (e: Exception) {
+        println(e)
+        exitProcess(1)
+    }
 }
